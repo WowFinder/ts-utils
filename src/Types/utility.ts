59b4167090d803eebba6cd668e5912ce29873237
Edurne Pascual <herenvardo@gmail.com>
2025-05-10 01:51:55 +0200

    minor: add Transform generic type

--- src/Types/utility.ts
+++ src/Types/utility.ts
@@ -8,29 +8,21 @@ type Expanded<T extends { [key: string]: V }, V> = T & { ''?: V };
 
 type Validator<T> = (value: T) => boolean;
 
+type Transform<T, U = T> = (value: T) => U;
+
 type PossiblyString<T> = Exclude<T, string> | string;
 
 type NotNill<T> = T extends null | undefined ? never : T;
 
 type Primitive = undefined | null | boolean | string | number | Function;
 
-type DeepRequired<T> = T extends Primitive
-    ? NotNill<T>
-    : {
-          [P in keyof T]-?: T[P] extends Array<infer U>
-              ? Array<DeepRequired<U>>
-              : T[P] extends ReadonlyArray<infer U2>
-                ? DeepRequired<U2>
-                : DeepRequired<T[P]>;
-      };
-
 export type {
     NonPrivate,
     Optional,
     Expanded,
     Validator,
+    Transform,
     PossiblyString,
     NotNill,
     Primitive,
-    DeepRequired,
 };
